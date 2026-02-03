c9e32f6eed3966d0c6ec9de7afff9801754069b8
desertwitch <24509509+desertwitch@users.noreply.github.com>
2026-02-03 13:26:03 +0100

    chore(par2): sane consts

--- internal/par2/parse.go
+++ internal/par2/parse.go
@@ -29,17 +29,17 @@ const (
 	mainSizeFixed     = 12 // SliceSize(8) + NumFiles (4)
 	fileDescSizeFixed = 56 // FileID(16) + HashFull(16) + Hash16k(16) + Length(8)
 
-	maxSets           = 10       // Sane amount of sets
-	maxIDsPerSet      = 100000   // Sane amount of IDs per set
-	maxFilesPerSet    = 100000   // Sane amount of files per set
-	maxPacketSize     = 10 << 20 // Sane packet size (10 MB)
-	maxFilenameLength = 65535    // Sane filename length
+	maxSets           = 10               // Sane amount of sets
+	maxIDsPerSet      = 100000           // Sane amount of IDs per set
+	maxFilesPerSet    = 100000           // Sane amount of files per set
+	maxPacketSize     = 10 * 1024 * 1024 // Sane packet size (10 MiB)
+	maxFilenameLength = 65535            // Sane filename length
 
 	packetHashOffset = 32 // Starting offset for MD5 hashing
 	packetHeaderSize = 64 // Total header size of a packet in bytes
 
-	recoverBufferSize   = 16384 // Next packet search uses 16KB chunks for reads
-	recoverStallRetries = 10    // Next packet search can stall for up to 10 times
+	recoverBufferSize   = 256 * 1024 // Next packet search reads in 256KiB chunks
+	recoverStallRetries = 10         // Next packet search can stall for up to 10 times
 )
 
 var (
