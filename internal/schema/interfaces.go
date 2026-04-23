1fe7e7a1bb80ac1bc8736fde4de506fd190b75bf
desertwitch <24509509+desertwitch@users.noreply.github.com>
2026-04-23 10:59:07 +0200

    chore: remove comment

--- internal/schema/interfaces.go
+++ internal/schema/interfaces.go
@@ -10,7 +10,6 @@ import (
 	"github.com/spf13/afero"
 )
 
-// FilesystemWalker is an interface describing a filesystem walking function.
 type FilesystemWalker interface {
 	Name() string
 	WalkDir(root string, fn fs.WalkDirFunc) error
